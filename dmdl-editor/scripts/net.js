01908a8d73f164588d71849f4ac564e35fc57658
zluudg <zluudg@zluudg.xyz>
2019-01-07 14:57:44 +0100

    Code for adding wires to nets written, not tested

--- dmdl-editor/scripts/net.js
+++ dmdl-editor/scripts/net.js
@@ -1,17 +1,67 @@
 function Net() {
     this.uniqueID = "net_" + Net.netCount;
     Net.netCount++;
-    Net.netList.push(this);
 
-    this.portList = [];
+    this.wireList = [];
 }
+
+Net.prototype.addWire = function(wire) {
+    this.wireList.push(wire);
+}
+
+/*
+ * ################################################################################################
+ * ######################################### Static Stuff #########################################
+ * ################################################################################################
+ */
 
 Net.netCount = 0;
 Net.netList = [];
 
-Net.prototype.addWire = function(wire) {
-    this.portList.push(wire.startPort);
-    this.portList.push(wire.endPort);
+Net.addWireToNet = function(wire) {
+    var foundNet = false;
+
+    for (var n=0; n<Net.netList.length-1; n++) {
+        for (var w=0; w<n.wireList.length-1; w++) {
+            if (Net.isSameNet(w, wire)) {
+                n.addWire(wire);
+                foundNet = true;
+                break;
+            }
+        }
+        if (foundNet)
+            break;
+    }
+
+    if (!foundNet) {
+        Net.createNewNet(wire);
+    }
+}
+
+Net.isSameNet = function(w1, w2) {
+    if (w1.startPort == w2.startPort) {
+        return true;
+    }
+
+    if (w1.endPort == w2.startPort) {
+        return true;
+    }
+
+    if (w1.startPort == w2.endPort) {
+        return true;
+    }
+
+    if (w1.endPort == w2.endPort) {
+        return true;
+    }
+
+    return false;
+}
+
+Net.createNewNet = function(wire) {
+    var net = new Net();
+    net.addWire(wire);
+    Net.netList.push(net);
 }
 
 Net.exportXML = function() {
