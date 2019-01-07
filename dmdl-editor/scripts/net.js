e0a2b33f553cbde02a8b1ba45c53096b925927ad
zluudg <zluudg@zluudg.xyz>
2019-01-07 09:34:48 +0100

    Expanded stub a bit

--- dmdl-editor/scripts/net.js
+++ dmdl-editor/scripts/net.js
@@ -1,6 +1,19 @@
 function Net() {
     this.uniqueID = "net_" + Net.netCount;
     Net.netCount++;
+    Net.netList.push(this);
+
+    this.portList = [];
 }
 
 Net.netCount = 0;
+Net.netList = [];
+
+Net.prototype.addWire = function(wire) {
+    this.portList.push(wire.startPort);
+    this.portList.push(wire.endPort);
+}
+
+Net.exportXML = function() {
+
+}
